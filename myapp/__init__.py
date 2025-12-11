e1719260b77113ffb28d56c983f00dda495f3d79
manigun03 <manigun03@gmail.com>
2025-12-11 14:40:10 +0900

    Feat: 기본 소스 파일 및 테스트 환경 구성   - myapp

---
+++ myapp/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import config
+
+db = SQLAlchemy()
+
+def create_app(config_name='default'):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+
+    db.init_app(app)
+
+    from myapp.auth import auth_bp
+    app.register_blueprint(auth_bp, url_prefix='/auth')
+
+    from myapp.api import api_bp
+    app.register_blueprint(api_bp, url_prefix='/api')
+
+    from myapp.main import main_bp
+    app.register_blueprint(main_bp, url_prefix='/')
+
+    return app
